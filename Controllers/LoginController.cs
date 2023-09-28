2d9803c47c4fbdae25d91c64316be0a91261ef75
Matias Alicata <matiasalicata@gmail.com>
2023-09-28 00:48:53 -0300

    initial commit

---
+++ Controllers/LoginController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace TechOilFrontend.Controllers
+{
+	public class LoginController : Controller
+	{
+		public IActionResult Login()
+		{
+			return View();
+		}
+		public IActionResult Ingresar()
+		{
+			return View();
+		}
+	}
+}
