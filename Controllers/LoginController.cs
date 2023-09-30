ae3679c877889efe124f18c8c7eb260f21ee566a
Matias Alicata <matiasalicata@gmail.com>
2023-09-30 13:23:46 -0300

    agregar PostToApi

--- Controllers/LoginController.cs
+++ Controllers/LoginController.cs
@@ -1,17 +1,31 @@
-﻿using Data.DTOs;
+﻿using Data.Base;
+using Data.DTOs;
 using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using System.Text.Json;
+using Microsoft.AspNetCore.Mvc.ViewFeatures;
 
 namespace TechOilFrontend.Controllers
 {
 	public class LoginController : Controller
 	{
-		public IActionResult Login()
+		private readonly System.Net.Http.IHttpClientFactory _httpClientFactory;
+        public LoginController(IHttpClientFactory httpClient)
+        {
+            _httpClientFactory = httpClient;
+        }
+        public IActionResult Login()
 		{
 			return View();
 		}
-		public IActionResult Ingresar(LoginDto login)
+		public async Task<IActionResult> Ingresar(LoginDto login)
 		{
-			return View("~/Views/Home/Index.cshtml");
+			var baseApi = new BaseApi(_httpClientFactory);
+			var token = await baseApi.PostToApi("Login", login);
+			var resultadoLogin = token as OkObjectResult;
+			var resultadoObjeto = JsonConvert.DeserializeObject<Models.Login>(resultadoLogin.Value.ToString());
+			ViewBag.Nombre = resultadoObjeto.Name;
+			return View("~/Views/Home/Index.cshtml", resultadoObjeto);
 		}
 	}
 }
